7121c74ba919b4893d078520acfbe203d8159d9b
Usama Ashraf <usamaashraf47@yahoo.com>
2025-10-13 12:19:02 -0500

    Implemented jwt token generation in auth controller

--- DotnetAPI/DTO/UserForRegistrationDTO.cs
+++ DotnetAPI/DTO/UserForRegistrationDTO.cs
@@ -2,7 +2,10 @@
 {
     public partial class UserForRegistrationDTO
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = "";
         public string Email { get; set; } = "";
+        public string Gender { get; set; } = "";
         public string Password { get; set; } = "";
         public string PasswordConfirm { get; set; } = "";
     }
